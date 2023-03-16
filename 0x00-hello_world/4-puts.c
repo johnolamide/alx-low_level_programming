6e4e9229440735ad4b5c4fba9e45d273e35e927c
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-16 08:03:49 -0700

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* main function using the puts function */
+/** 
+ * main function using the puts function 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
