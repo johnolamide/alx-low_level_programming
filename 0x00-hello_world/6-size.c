f40db538b0b587765f2e24f25498da1989c1af07
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-16 08:45:56 -0700

    add 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (long)sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", (long)sizeof(long long int));
+	printf("Size of a long long int: %lu byte(s)\n", (long)sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (long)sizeof(float));
 
 	return (0);
