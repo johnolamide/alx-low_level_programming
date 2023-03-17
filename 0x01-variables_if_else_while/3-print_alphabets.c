2f511d849e34f6eb747bdc45b842e6a7a7671d8d
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-17 07:05:16 -0700

    add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - prints alphabet in lower and upper case
+ * Retun: returns 0 for SUCCESS
+ */
+int main(void)
+{
+	char c;
+	char u;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(a);
+
+	for (u = 'A'; u <= 'Z'; u++)
+		putchar(u);
+
+	putchar('\n');
+
+	return (0);
+}
