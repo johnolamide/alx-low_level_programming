cc531e651ab2d5aaf5b1f7154224e7613c48c520
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-17 04:49:42 -0700

    add 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 /**
  * main - checks if random number is positve,
  * nagative or zero.
