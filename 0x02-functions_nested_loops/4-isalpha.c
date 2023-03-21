cce609e29a4f73449ba8f04dae2107f3c61cd6f0
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-21 05:10:29 -0700

    add 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ * @c: the character to check
+ *
+ * Return:
+ *	1 if alphabet
+ *	0 if otherwise
+ */
+int _isalpha(int c)
+{
+	char a;
+	a = (char)c;
+
+	if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z'))
+		return(1);
+	else
+		return (0);
+}
