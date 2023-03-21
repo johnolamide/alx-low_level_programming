bc1639fab565ed9efffc29b0727ec59bf612f474
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-21 04:33:01 -0700

    add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: returns 0 for SUCCESS
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
