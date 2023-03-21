33840cbdeaf49e6fa83f2f51a66d8f145e5cf1b9
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-21 09:08:12 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_to_98 - prints to 98
+ * @n: the number to print from
+ * Return: returns void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		int i;
+
+		for (i = n; i >= 98; i--)
+		{
+			_putchar('0' + i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				continue;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		int i;
+
+		for (i = n; i <= 98; i++)
+		{
+			_putchar('0' + i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				continue;
+			_putchar('\n');
+		}
+	}
+}
