9a32d247d37f2903768bc8679b8dd5eaee93d400
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-21 08:34:50 -0700

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers together
+ * @i: first operand
+ * @j: second operand
+ *
+ * Return: returns the sum of two operand
+ */
+int add (int i, int j)
+{
+	int r;
+
+	r = i + j;
+
+	return (r);
+}
