77488f5d3393c106721c6d8d2a9fcee9b557b554
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-21 08:29:10 -0700

    add 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (i = 0; i <= 9; i++)
 		{
 			if (i == 0)
-				_putchar('');
+				_putchar('\0');
 			else if (((n * i) / 10) != 0)
 				_putchar('0' + ((n * i) / 10));
 			else
