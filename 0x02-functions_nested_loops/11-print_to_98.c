fca504d1943810c73d9db3e3189008d2b92ea4fa
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-21 19:13:53 -0700

    add 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,33 @@ int _abs(int n)
 }
 
 /**
+ * negative_values - prints out negative values up to 98
+ * @n: number to start from
+ * Return: returns the values to be printed
+ */
+void negative_values(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i < 0)
+			_putchar('-');
+		if (_abs(i) >= 100)
+			_putchar('0' + (_abs(i) / 100));
+		if (_abs(i) >= 10)
+			_putchar('0'+ ((_abs(i) / 10) % 10));
+		_putchar('0' + (_abs(i) % 10));
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
+
+/**
  * print_to_98 - prints to 98
  * @n: the number to print from
  * Return: returns void
@@ -22,24 +49,7 @@ void print_to_98(int n)
 {
 	if (n < 0)
 	{
-		int i;
-
-		for (i = n; i <= 98; i++)
-		{
-			if (i < 0)
-				_putchar('-');
-			if (_abs(i) >= 100)
-				_putchar('0' + (_abs(i) / 100));
-			if (_abs(i) >= 10)
-				_putchar('0' + ((_abs(i) / 10) % 10));
-			_putchar('0' + (_abs(i) % 10));
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
+		negative_values(n);
 	}
 	else if (n >= 98)
 	{
