979cb14ecd272d476744c2014a1689e2f8bdfcb5
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-23 10:14:45 -0700

    add 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,9 +5,14 @@
  */
 void print_line(int n)
 {
-	int i;
+	if (n > 0)
+	{
+		int i;
 
-	for (i = 0; i <= n, i++)
-		_putchar('_');
-	_putchar('\n');
+		for (i = 0; i <= n, i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
 }
