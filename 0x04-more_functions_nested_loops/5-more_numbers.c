3383f5f094081e27f1c73f9c5a709288dbec4961
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-23 06:54:44 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print 0-14 ten times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		int i;
+
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar(('0' + (i / 10) % 10));
+			_putchar('0' + i);
+		}
+		_putchar('\n');
+	}
+}
