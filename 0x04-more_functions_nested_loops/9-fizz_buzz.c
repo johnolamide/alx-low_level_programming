aaea01d360fb08bdd9d74bb4b894d2dbe5631456
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-23 12:55:35 -0700

    add 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-		{
-			if ((i % 15) == 0)
-				printf("FizzBuzz ");
-			else if ((i % 3) == 0)
-				printf("Fizz ");
-			else
-				printf("Buzz ");
-		}
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz\n");
+		else if ((i % 3) == 0)
+			printf("Fizz\n");
+		else if ((i % 5) == 0)
+			printf("Buzz\n");
 		else
-			printf("%d ", i);
+			printf("%d\n");
 	}
-	printf("\n");
-
 	return (0);
 }
