c7c1a154e3329a51c5af8cfe895768c8098f56e7
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-23 14:42:31 -0700

    add 0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
 #include <stdio.h>
 
@@ -9,4 +9,4 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
 
-#endif /* MAIN_H */
+#endif
