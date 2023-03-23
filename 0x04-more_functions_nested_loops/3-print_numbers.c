6f55da2526acd6e7e21d4b6cb6ad82009ed75cf8
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-23 06:12:17 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
