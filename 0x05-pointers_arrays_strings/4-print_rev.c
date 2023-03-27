07022a67a04efce01ced913a669b192b2107a105
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-27 08:32:21 -0700

    add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: pointer to the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar('%c', *(s + i));
+
+	_putchar('\n');
+}
