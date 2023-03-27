336a062d09558447c5a7ec650015b1889590ecca
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-27 09:54:53 -0700

    add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - print the last half of a string
+ * @str: string pointer
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+
+	if (len % 2 == 0)
+	{
+		int i;
+
+		for (i = (len / 2); i <= len - 1; i++)
+			_putchar(*(str + i));
+	}
+	else
+	{
+		int i;
+
+		for (i = ((len - 1) / 2); i <= len - 1; i++)
+			_putchar(*(str + i));
+	}
+
+	_putchar('\n');
+}
