a08659a3d1c1e325695048e58aabe187e57845ec
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-27 09:35:39 -0700

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: the string pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+
+	for (i = 0; i <= len - 1; i++)
+	{
+		if (i % 2 == )
+			_putchar(*(str + i));
+	}
+
+	_putchar('\n');
+}
