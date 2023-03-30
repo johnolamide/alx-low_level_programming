8637c41b75f95dcc69b32bc60a07d8bf5876e52f
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-30 00:11:41 -0700

    add 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - copies string from src to destination
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n : control integer
+ * Return: return destination pointer 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
