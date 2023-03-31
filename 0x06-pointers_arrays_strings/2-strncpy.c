56174a8b5b4e3d103ef8aba75161015a760de4c0
John Olamide Alokan <alokanolamide@gmail.com>
2023-03-31 13:03:55 -0700

    add 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * @dest: destination pointer
  * @src: source pointer
  * @n : control integer
- * Return: return destination pointer 
+ * Return: return destination pointer
  */
 char *_strncpy(char *dest, char *src, int n)
 {
