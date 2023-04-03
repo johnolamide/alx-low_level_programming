f14db87b95a73bd0e441fa19a97bd2948bacce0c
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-03 04:28:44 -0700

    add  0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,15 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	char **ps;
+	int i;
+
+	i = 0;
 	ps = &s;
 
-	while (n > 0)
+	while (i <= n)
 	{
-		s++ = b;
-		n++;
+		*s++ = b;
+		i++;
 	}
 	
 	return (*ps);
