723778e5bc62ba38b0b87c8043809ad23275209e
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-03 15:50:11 -0700

    add 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
+        while (*s != '\0')
+        {
+                if (*s == c)
+                        return (s);
 
-		s++;
-	}
-	return NULL;
+                s++;
+        }
+        return (NULL);
 }
