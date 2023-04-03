108a81f3bf0f25f7a56036b3adf7cefde3592996
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-03 15:28:31 -0700

    add 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer string to search 
+ * @needle: pointer substring to find
+ * Return: returns pointer or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h;
+	char *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*n == *haystack && *n != '\0' && *haystack != '\0')
+		{
+			n++;
+			haystack++;
+		}
+
+		if (*n == '\0')
+		{
+			return (h);
+		}
+
+		haystack = h + 1;
+	}
+
+	return (NULL);
+}
