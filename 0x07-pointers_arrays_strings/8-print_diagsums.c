7870ea02a9b34b6ec0c4e788ff3f53804877278e
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-04 03:16:45 -0700

    add 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,12 +14,10 @@ void print_diagsums(int *a, int size)
 
 	sum1 = 0;
 	sum2 = 0;
-
 	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + i * size + i);
 		sum2 += *(a + i * size + (size - 1 - i));
 	}
-
 	printf("%d, %d\n", sum1, sum2);
 }
