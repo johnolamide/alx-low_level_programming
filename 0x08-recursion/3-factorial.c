155c1c743ea2a8c91ccfcdf592b5cf5be4a6dd7e
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-05 02:30:51 -0700

    add 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * factorial - calculates the factori of a number
+ * @n: number
+ * Return: factorial value
+ */
+int factorial(int n)
+{
+	if (n <= 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
