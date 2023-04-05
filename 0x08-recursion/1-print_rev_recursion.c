2e6839455168ac3267d0cdb2240b9243f815d7c1
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-05 00:31:33 -0700

    add 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: pointer to string 
+ * @s: pointer to string
  * Return: void
  */
 void _print_rev_recursion(char *s)
