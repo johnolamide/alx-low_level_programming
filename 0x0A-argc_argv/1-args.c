ff7366a5ea54760c4bd33f957485b56fd2d73b2d
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-09 11:18:59 -0700

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return 0 for SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	_putchar('0' + argc);
+	_putchar('\n');
+	return (0);
+}
