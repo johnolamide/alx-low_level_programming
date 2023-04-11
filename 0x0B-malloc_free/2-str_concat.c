a694a189cadb26a8693f83f00a0da3d0df1985f5
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-11 13:32:58 -0700

    add 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * str_concat - concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1;
+	size_t len2;
+	char *result;
+	char *q;
+	char *p1;
+	char *p2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = 0;
+	len2 = 0;
+	p1 = s1;
+	p2 = s2
+
+	while (*p1 != '\0')
+	{
+		len1++;
+		p1++;
+	}
+
+	while (*p2 != '\0')
+	{
+		len2++;
+		p2++;
+	}
+	result = (char *) malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (result == NULL)
+		return NULL;
+
+	q = result;
+
+	while (*s1 != '\0')
+		*q++ = *s1++;
+
+	while (*s2 != '\0')
+		*q++ = *s2++;
+
+	*q = '\0';
+
+	return result;
+}
