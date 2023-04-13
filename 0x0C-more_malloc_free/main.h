e50dbd36a60bb60a805c406a70d56da238d97fc7
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-13 12:58:25 -0700

    add 101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void*_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_number(char *str);
+int main(int argc, char *argv[]);
 
 #endif
