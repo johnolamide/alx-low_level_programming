abb523acd6f3fad7cd5600f2de5ab0793dd4ae9c
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-13 12:36:09 -0700

    add 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (alm == NULL)
 		return (NULL);
-	for (i = 0, i < (nmemb * size); i++)
+	for (i = 0; i < (nmemb * size); i++)
 		alm[i] = 0;
 
 	return (alm);
