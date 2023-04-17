6f184010a5dbd02c2197680c43bc0e798e84f668
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-17 12:04:43 -0700

    add 1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - dog structure
  * @name: name of the dog
@@ -12,5 +16,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
