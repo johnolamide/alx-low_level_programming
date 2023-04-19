bc6128d95815d5215a7cde956363b4f6232bf341
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-19 13:11:23 -0700

    add 3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - sum function
+ * @a: first variable
+ * @b: second variable
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference function
+ * @a: first variable
+ * @b: second variable
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product function
+ * @a: first variable
+ * @b: second variable
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division function
+ * @a: first variable
+ * @b: second variable
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder function
+ * @a: first variable
+ * @b: second variable
+ * Return: remainder of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
