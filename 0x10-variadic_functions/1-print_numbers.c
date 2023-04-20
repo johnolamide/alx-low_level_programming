55583a57101c3083cdd1a77d337f88e9c89572f0
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-20 06:35:23 -0700

    add 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
-		printf("%d%c", num, separator);
+		printf("%d%s", num, separator);
 	}
 	printf("\n");
 }
