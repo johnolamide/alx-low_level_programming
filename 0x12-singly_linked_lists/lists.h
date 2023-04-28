37e3d35f0e3cf9de65c4efeedb41e130eec22d82
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-27 23:53:58 -0700

    add 0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'd string)
+ * @len: length of the string
+ * @next: pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
