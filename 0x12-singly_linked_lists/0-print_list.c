d97c5b555a8ea6f3412d1b0a663d623372be2db9
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-28 00:20:44 -0700

    [FIXED] 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL || h->len == 0)
+		if (h->str == "" || h->str == NULL || h->len == 0)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] (%s)\n", h->len, h->str);
