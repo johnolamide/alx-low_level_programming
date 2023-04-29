79c6fc32ae11c91ce77131d0da1d80c79af602d4
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-29 07:18:39 -0700

    [ADD] 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints all elements of a list_t list
+ * @h: listint_t list pointer
+ * Return: number of elements in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (NULL);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
