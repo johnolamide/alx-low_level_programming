fc7f1ccd07f78d241ae80d453c29d9b1ddbd3a7d
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-29 08:25:19 -0700

    [ADD] 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,8 +7,8 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t node;
-	listint_t temp;
+	listint_t *node;
+	listint_t *temp;
 
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
