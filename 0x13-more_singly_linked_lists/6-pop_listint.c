2f40658f65296e62bb0f9acc23d344e8526f6f17
John Olamide Alokan <alokanolamide@gmail.com>
2023-04-30 12:26:31 -0700

    [ADD] 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head listint_t list
+ * @head: head of the listint_t list
+ * Return: returns the listint_t data n
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	return (n);
+}
