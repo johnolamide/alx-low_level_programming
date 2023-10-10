0159cd9535c13adb67596cbdc75c6e2350089131
Olamide Alokan <alokanolamide@gmail.com>
2023-10-10 19:38:45 +0100

    [ADD] 101-O

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /** PROTOTYPE **/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t left, size_t right);
+int jump_search(int *array, size_t size, int value);
 
 #endif /** SEARCH_ALGOS_H **/
